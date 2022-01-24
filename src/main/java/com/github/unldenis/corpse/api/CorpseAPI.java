e4adbcc819256ddc872834c0fb7643ce4af31b52
unldenis <mehillideny13@gmail.com>
2022-01-24 19:01:05 +0100

    added javadoc

--- src/main/java/com/github/unldenis/corpse/api/CorpseAPI.java
+++ src/main/java/com/github/unldenis/corpse/api/CorpseAPI.java
@@ -31,26 +31,55 @@ public class CorpseAPI {
 
     private static CorpseAPI instance;
 
-    private CorpseAPI() {}
+    public CorpseAPI() {
+        throw new IllegalArgumentException();
+    }
 
+    /**
+     * Method that creates a corpse in the player's position and with its skin and inventory
+     * @param player The player to copy
+     * @return a new Corpse object
+     */
     public Corpse spawnCorpse(@NotNull Player player) {
         Validate.notNull(player, "Player cannot be null");
         return new Corpse(player);
     }
 
+    /**
+     * Method that creates a corpse in the given place and with the skin, name and inventory of the player
+     * @param player The player to copy
+     * @param location The location where to spawn the corpse
+     * @return a new Corpse object
+     */
     public Corpse spawnCorpse(@NotNull Player player, @NotNull Location location) {
         Validate.notNull(player, "Player cannot be null");
         Validate.notNull(location, "Spawn location cannot be null");
         return new Corpse(location, WrappedGameProfile.fromPlayer(player), null, player.getName());
     }
 
-
+    /**
+     * Method that creates a corpse in the given place and with the skin and name of the offlinePlayer
+     * @param offlinePlayer The offlinePlayer to copy
+     * @param location The location where to spawn the corpse
+     * @return a new Corpse object
+     */
     public Corpse spawnCorpse(@NotNull OfflinePlayer offlinePlayer, @NotNull Location location) {
         Validate.notNull(offlinePlayer, "OfflinePlayer cannot be null");
         Validate.notNull(location, "Spawn location cannot be null");
         return new Corpse(location, offlinePlayer, null);
     }
 
+    /**
+     * Method that creates a corpse in the given place and with the skin and name of the player
+     * with a custom inventory.
+     * @param player The player to copy
+     * @param location The location where to spawn the corpse
+     * @param helmet The helmet to put on the corpse
+     * @param chestPlate The chestPlate to put on the corpse
+     * @param leggings The leggings to put on the corpse
+     * @param boots The boots to put on the corpse
+     * @return a new Corpse object
+     */
     public Corpse spawnCorpse(
             @NotNull Player player,
             @NotNull Location location,
@@ -64,6 +93,17 @@ public class CorpseAPI {
         return new Corpse(location, WrappedGameProfile.fromPlayer(player), new ItemStack[]{boots, leggings, chestPlate, helmet}, player.getName());
     }
 
+    /**
+     * Method that creates a corpse in the given place and with the skin and name of the offlinePlayer
+     * with a custom inventory.
+     * @param offlinePlayer The offlinePlayer to copy
+     * @param location The location where to spawn the corpse
+     * @param helmet The helmet to put on the corpse
+     * @param chestPlate The chestPlate to put on the corpse
+     * @param leggings The leggings to put on the corpse
+     * @param boots The boots to put on the corpse
+     * @return a new Corpse object
+     */
     public Corpse spawnCorpse(
             @NotNull OfflinePlayer offlinePlayer,
             @NotNull Location location,
@@ -77,11 +117,19 @@ public class CorpseAPI {
         return new Corpse(location, offlinePlayer, new ItemStack[]{boots, leggings, chestPlate, helmet});
     }
 
+    /**
+     * Method that removes a corpse
+     * @param corpse The corpse to be removed
+     */
     public void removeCorpse(@NotNull Corpse corpse) {
         Validate.notNull(corpse, "Corpse cannot be null");
         CorpsePool.getInstance().remove(corpse.getId());
     }
 
+    /**
+     * Class method that allows you to use the API.
+     * @return an instance of this class
+     */
     @NotNull
     public static synchronized CorpseAPI getInstance() {
         if(instance == null) {
